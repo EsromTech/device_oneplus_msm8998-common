1f9b3409802c5b41e90190ec37147186ec02d57b
EsromTech <esromgalaxy5@gmail.com>
2018-06-06 17:53:18 -0300

    Use kernel RenderFlash with Clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,8 +76,9 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
-TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
+TARGET_KERNEL_CONFIG := flash-custom_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_PATH := $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-4053586/bin
 
 # Sepolicy
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
